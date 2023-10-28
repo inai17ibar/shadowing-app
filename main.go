15539e1e7b67617f9a60fc2312ba9575c190f902
Soichiro Inatani <inai17ibar@gmail.com>
2023-10-28 15:36:07 +0900

    ID、タイトル、サムネイルを取得するように改良した。

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"google.golang.org/api/googleapi/transport"
-	"google.golang.org/api/youtube/v3"
 )
 
 func main() {
@@ -20,41 +21,67 @@ func main() {
 	r.Use(cors.Default())
 
 	r.GET("/search", func(c *gin.Context) {
-		query := c.Query("q")
-		results, err := searchYoutube(query)
+		query := c.DefaultQuery("q", "")
+
+		apiKey := os.Getenv("YOUTUBE_DATA_API_KEY")
+		endpoint := "https://www.googleapis.com/youtube/v3/search"
+		fullURL := endpoint + "?part=snippet&type=video&maxResults=5&q=" + query + "&key=" + apiKey
+
+		resp, err := http.Get(fullURL)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			log.Println("Failed to fetch data from YouTube:", err)
+			c.JSON(500, gin.H{"error": "Failed to fetch data from YouTube"})
 			return
 		}
-		c.JSON(http.StatusOK, results)
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Println("Failed to read YouTube response:", err)
+			c.JSON(500, gin.H{"error": "Failed to read YouTube response"})
+			return
+		}
+
+		var youtubeResponse YoutubeResponse
+		err = json.Unmarshal(body, &youtubeResponse)
+		if err != nil {
+			log.Println("Failed to parse YouTube API response:", err)
+			c.JSON(500, gin.H{"error": "Failed to parse YouTube API response"})
+			return
+		}
+
+		// 必要な情報を抽出してフロントエンドに渡す
+		// 必要な情報を抽出してフロントエンドに渡す
+		videos := []map[string]string{}
+		for _, item := range youtubeResponse.Items {
+			video := map[string]string{
+				"id":        item.Id.VideoId,
+				"title":     item.Snippet.Title,
+				"thumbnail": item.Snippet.Thumbnails.Default.URL,
+			}
+			//log.Print(item.Id.VideoId)
+			videos = append(videos, video)
+		}
+
+		c.JSON(200, gin.H{"results": videos})
 	})
 
 	r.Run(":8080")
 }
 
-func searchYoutube(query string) ([]string, error) {
-	apiKey := os.Getenv("YOUTUBE_DATA_API_KEY")
-
-	client := &http.Client{
-		Transport: &transport.APIKey{Key: apiKey},
-	}
-
-	service, err := youtube.New(client)
-	if err != nil {
-		return nil, err
-	}
-
-	call := service.Search.List([]string{"id", "snippet"}).Q(query).MaxResults(10)
-	response, err := call.Do()
-	if err != nil {
-		return nil, err
-	}
-
-	var videoIDs []string
-	for _, item := range response.Items {
-		if item.Id.Kind == "youtube#video" {
-			videoIDs = append(videoIDs, item.Id.VideoId)
-		}
-	}
-	return videoIDs, nil
+// 応答の構造体
+type YoutubeResponse struct {
+	Items []struct {
+		Id struct {
+			VideoId string `json:"videoId"`
+		} `json:"id"`
+		Snippet struct {
+			Title      string `json:"title"`
+			Thumbnails struct {
+				Default struct {
+					URL string `json:"url"`
+				} `json:"default"`
+			} `json:"thumbnails"`
+		} `json:"snippet"`
+	} `json:"items"`
 }
